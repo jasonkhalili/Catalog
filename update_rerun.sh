3cd764d1b70edeadbf778719f1fb1d3c96f31bfd
jasonk <jasonkhalili@gmail.com>
2022-12-30 11:55:54 -0800

    Added files via upload

--- update_rerun.sh
+++
@@ -1,7 +0,0 @@
-# cd Python-Scripts/csun_catalog_and_schedules_bot/
-# git fetch && git pull
-# mv --force bot.py ../../
-# cd ~
-# python3 bot.py
-
-while :; do git fetch && git pull && node bot.js; done
